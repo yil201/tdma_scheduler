71301c5cf471df47be89787bbc67e5874c9ab47a
yil201 <yil201@pitt.edu>
2023-03-21 01:06:17 -0400

    Update kmod_netfilter_tcp_send.c

--- kernel-space/kmod_netfilter_tcp_send.c
+++ kernel-space/kmod_netfilter_tcp_send.c
@@ -1,54 +1,122 @@
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
-#include <linux/skbuff.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
 
-static struct nf_hook_ops nfho; // netfilter hook function to intercept outgoing packets
+static struct nf_hook_ops nfho;
 
+// TCP packet sending function
+int send_tcp_packet(void)
+{
+    struct socket *sock;
+    struct sockaddr_in sin;
+    struct msghdr msg;
+    struct iovec iov;
+    int len, ret;
+
+    // Create a socket
+    sock = (struct socket *)sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &ret);
+    if (!sock) {
+        printk(KERN_ALERT "Error creating socket\n");
+        return -1;
+    }
+
+    // Fill in the destination address
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // example: send to loopback interface
+    sin.sin_port = htons(80); // example: send to port 80
+
+    // Connect to the destination address
+    ret = sock->ops->connect(sock, (struct sockaddr *)&sin, sizeof(sin), 0);
+    if (ret < 0) {
+        printk(KERN_ALERT "Error connecting to destination address\n");
+        sock_release(sock);
+        return -1;
+    }
+
+    // Prepare the TCP packet
+    len = sizeof(struct iphdr) + sizeof(struct tcphdr) + 1; // example: TCP packet size
+    char buf[len];
+    memset(buf, 0, len);
+    struct iphdr *ip_header = (struct iphdr *)buf;
+    struct tcphdr *tcp_header = (struct tcphdr *)(buf + sizeof(struct iphdr));
+    ip_header->protocol = IPPROTO_TCP;
+    ip_header->saddr = in_aton("192.168.1.1"); // example: source IP address
+    ip_header->daddr = sin.sin_addr.s_addr;
+    tcp_header->source = htons(1234); // example: source port
+    tcp_header->dest = sin.sin_port;
+    tcp_header->seq = htonl(1); // example: sequence number
+    tcp_header->ack_seq = 0;
+    tcp_header->doff = sizeof(struct tcphdr) / 4;
+    tcp_header->fin = 0;
+    tcp_header->syn = 1;
+    tcp_header->rst = 0;
+    tcp_header->psh = 0;
+    tcp_header->ack = 0;
+    tcp_header->urg = 0;
+    tcp_header->window = htons(65535); // example: window size
+    tcp_header->check = 0;
+    tcp_header->urg_ptr = 0;
+
+    // Send the TCP packet
+    iov.iov_base = buf;
+    iov.iov_len = len;
+    msg.msg_flags = 0;
+    msg.msg_name = &sin;
+    msg.msg_namelen = sizeof(sin);
+    msg.msg_control = NULL;
+    msg.msg_controllen = 0;
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    ret = sock_sendmsg(sock, &msg, len);
+    if (ret < 0) {
+        printk(KERN_ALERT "Error sending TCP packet\n");
+        sock_release(sock);
+        return -1;
+    }
+
+    sock_release(sock);
+
+    return 0;
+}
+
+// Netfilter hook function
 unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-    struct iphdr *ip_header = (struct iphdr *) skb_network_header(skb);
-    struct tcphdr *tcp_header = (struct tcphdr *) skb_transport_header(skb);
-
-    // check if the packet is a TCP packet
-    if (ip_header->protocol == IPPROTO_TCP)
-    {
-        // do something with the packet
-        // ...
-
-        // send out the packet immediately
-        skb_dst_drop(skb);
-        nf_send(skb, state->net, state->pf, 0);
-
-        return NF_STOLEN;
+    if (send_tcp_packet() < 0) {
+        printk(KERN_ALERT "Error sending TCP packet\n");
     }
 
     return NF_ACCEPT;
 }
 
-// module init
-static int __init init_hook(void)
+static int __init init_module(void)
 {
-    // netfilter hook init
-    nfho.hook = hook_func;                  
-    nfho.hooknum = NF_INET_LOCAL_OUT;        
-    nfho.pf = PF_INET;                      
-    nfho.priority = NF_IP_PRI_FIRST;        
+    printk(KERN_INFO "TCP packet sender kernel module loaded\n");
 
-    // register the hook
-    nf_register_hook(&nfho);               
+    nfho.hook = hook_func;
+    nfho.hooknum = NF_INET_POST_ROUTING;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+    nf_register_hook(&nfho);
 
     return 0;
 }
 
-// module cleanup 
-static void __exit cleanup_hook(void)
+static void __exit cleanup_module(void)
 {
-    nf_unregister_hook(&nfho);              
+    printk(KERN_INFO "TCP packet sender kernel module unloaded\n");
+
+    nf_unregister_hook(&nfho);
 }
 
-module_init(init_hook);                     
-module_exit(cleanup_hook);                  
+module_init(init_module);
+module_exit(cleanup_module);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Enoch Li");
+MODULE_DESCRIPTION("Kernel module for sending a TCP packet out");
+
